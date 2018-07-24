892fa57b377d23ba9799d6d61bee2b524f6e8b6b
Vlad <vladgorbich@gmail.com>
2018-07-24 16:33:10 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join('__dirname', '../public');
+
+// For Heroku as prod
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+  console.log(`Server is running on port ${port}`);
+});
