bf29ab4b45cdbe2c4ed12c7936dca33e82eb3678
Vlad <vladgorbich@gmail.com>
2018-07-25 16:28:57 -0500

    add broadcasting events

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,20 @@
 var socket = io();
 
+// Log that connection was set successfully
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Andrew',
-    text: 'Yup, that works for me.'
-  });
 });
 
+// Log that disconnection happend
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
+// Log that a message from server received
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
 });
+
+/* To emit a custom event from console of the Google Dev tools:
+> socket.emit('createMessage', {from: 'Billy', text: 'This is Billy Bones'});
+*/
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
+
 // For Heroku deployment
 const port = process.env.PORT || 3000;
 var app = express();
@@ -12,19 +13,38 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+// Creating connection if request came 
+// to server from a user and log the message
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // Send data back to the user that just conected
   socket.emit('newMessage', {
-    from: 'John',
-    text: 'See you then',
-    createdAt: 123123
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
   });
 
+  // Send data back to all users that are connected 
+  // except the user that made the request 
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+  // Getting a message from a user and log it
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    // Send that data to all connected users
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
+  // Log a message if a user was disconnected
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
